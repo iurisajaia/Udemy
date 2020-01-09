0c22df5d8ee61b8cd4551b58e6a739ddc8deb3ec
iuri sajaia <iurisajaia@iuris-MacBook-Pro.local>
2020-01-10 01:07:55 +0400

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,220 @@
+@extends('views.welcome');
+@section('content')
+
+    <!--Slider Area Start-->
+    <div class="slider-area">
+        <div class="preview-2">
+            <div id="nivoslider" class="slides">
+                <img src="img/slider/1.jpg" alt="" title="#slider-1-caption1"/>
+            </div>
+            <div id="slider-1-caption1" class="nivo-html-caption nivo-caption">
+                <div class="banner-content slider-1">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-content-wrapper">
+                                    <div class="text-content">
+                                        <h1 class="title1 wow fadeInUp" data-wow-duration="2000ms" data-wow-delay="0s">Education Needs<br>
+                                        Complete Solution</h1>
+                                        <p class="sub-title wow fadeInUp d-none d-lg-block" data-wow-duration="2900ms" data-wow-delay=".5s"> There are many variations of passages of Lorem Ipsum available, but the majority have<br>
+                                        suffered alteration in some form, by injected humour, or randomised words which<br>
+                                        don't look even slightly believable.</p>
+                                        <div class="banner-readmore wow fadeInUp" data-wow-duration="3600ms" data-wow-delay=".6s">
+                                            <a class="button-default" href="#">View Courses</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of Slider Area-->
+    <!--Course Area Start-->
+    <div class="course-area section-padding bg-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title-wrapper">
+                        <div class="section-title">
+                            <h3>POPULAR COURSES</h3>
+                            <p>There are many variations of passages of Lorem Ipsum</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="single-item">
+                        <div class="single-item-image overlay-effect">
+                            <a href="#"><img src="img/course/1.jpg" alt=""></a>
+                        </div>
+                        <div class="single-item-text">
+                            <h4><a href="#">Photoshop CC 2017</a></h4>
+                            <div class="single-item-text-info">
+                                <span>By: <span>M S Nawaz</span></span>
+                                <span>Date: <span>20.5.15</span></span>
+                            </div>
+                            <p>There are many variations of sages of Lorem Ipsum available, but the mrity have suffered alteration in some orm, by injected humo ur,There are many but the mri have suffered alteration in some </p>
+                            <div class="single-item-content">
+                               <div class="single-item-comment-view">
+                                   <span><i class="zmdi zmdi-eye"></i>59</span>
+                                   <span><i class="zmdi zmdi-comments"></i>19</span>
+                               </div>
+                               <div class="single-item-rating">
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star-half"></i>
+                               </div>
+                            </div>
+                        </div>
+                        <div class="button-bottom">
+                            <a href="#" class="button-default">Learn Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="single-item">
+                        <div class="single-item-image overlay-effect">
+                            <a href="#"><img src="img/course/2.jpg" alt=""></a>
+                        </div>
+                        <div class="single-item-text">
+                            <h4><a href="#">Illustrator CC 2017</a></h4>
+                            <div class="single-item-text-info">
+                                <span>By: <span>Subas Das</span></span>
+                                <span>Date: <span>20.5.15</span></span>
+                            </div>
+                            <p>There are many variations of sages of Lorem Ipsum available, but the mrity have suffered alteration in some orm, by injected humo ur,There are many but the mri have suffered alteration in some </p>
+                            <div class="single-item-content">
+                               <div class="single-item-comment-view">
+                                   <span><i class="zmdi zmdi-eye"></i>59</span>
+                                   <span><i class="zmdi zmdi-comments"></i>19</span>
+                               </div>
+                               <div class="single-item-rating">
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star-half"></i>
+                               </div>
+                            </div>
+                        </div>
+                        <div class="button-bottom">
+                            <a href="#" class="button-default">Learn Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="single-item">
+                        <div class="single-item-image overlay-effect">
+                            <a href="#"><img src="img/course/3.jpg" alt=""></a>
+                        </div>
+                        <div class="single-item-text">
+                            <h4><a href="#">Indesign CC 2017</a></h4>
+                            <div class="single-item-text-info">
+                                <span>By: <span>Momin Boss</span></span>
+                                <span>Date: <span>20.5.15</span></span>
+                            </div>
+                            <p>There are many variations of sages of Lorem Ipsum available, but the mrity have suffered alteration in some orm, by injected humo ur,There are many but the mri have suffered alteration in some </p>
+                            <div class="single-item-content">
+                               <div class="single-item-comment-view">
+                                   <span><i class="zmdi zmdi-eye"></i>59</span>
+                                   <span><i class="zmdi zmdi-comments"></i>19</span>
+                               </div>
+                               <div class="single-item-rating">
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star"></i>
+                                   <i class="zmdi zmdi-star-half"></i>
+                               </div>
+                            </div>
+                        </div>
+                        <div class="button-bottom">
+                            <a href="#" class="button-default">Learn Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-12 text-center">
+                    <a href="#" class="button-default button-large">Browse All Courses <i class="zmdi zmdi-chevron-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of Course Area-->
+    
+    <!--Footer Widget Area Start-->
+    <div class="footer-widget-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html"><img src="img/logo/footer.png" alt=""></a>
+                        </div>
+                        <p>There are many variations of passg of Lorem Ipsum available, but thmajority have suffered altem, </p>
+                        <div class="social-icons">
+                            <a href="#"><i class="zmdi zmdi-facebook"></i></a>
+                            <a href="#"><i class="zmdi zmdi-rss"></i></a>
+                            <a href="#"><i class="zmdi zmdi-google-plus"></i></a>
+                            <a href="#"><i class="zmdi zmdi-pinterest"></i></a>
+                            <a href="#"><i class="zmdi zmdi-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-footer-widget">
+                        <h3>GET IN TOUCH</h3>
+                        <a href="tel:555-555-1212"><i class="fa fa-phone"></i>555-555-1212</a>
+                        <span><i class="fa fa-envelope"></i>info@example.com</span>
+                        <span><i class="fa fa-globe"></i>www.educat.com</span>
+                        <span><i class="fa fa-map-marker"></i>ur address goes here,street.</span>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-footer-widget">
+                        <h3>Useful Links</h3>
+                        <ul class="footer-list">
+                            <li><a href="#">Teachers &amp; Staff</a></li>
+                            <li><a href="#">Our Courses</a></li>
+                            <li><a href="#">Courses Categories</a></li>
+                            <li><a href="#">Support</a></li>
+                            <li><a href="#">Terms &amp; Conditions</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-footer-widget">
+                        <h3>Instagram</h3>
+                        <div class="instagram-image">
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/1.jpg" alt=""></a>
+                            </div>
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/2.jpg" alt=""></a>
+                            </div>
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/3.jpg" alt=""></a>
+                            </div>
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/4.jpg" alt=""></a>
+                            </div>
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/5.jpg" alt=""></a>
+                            </div>
+                            <div class="footer-img">
+                                <a href="#"><img src="img/footer/6.jpg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of Footer Widget Area-->
+@endsection
